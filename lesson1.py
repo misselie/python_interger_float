808e7108c8f62f079bc5307ec8e6b5103b136618
Elie <Pigglet@Elies-MacBook-Air.local>
2018-07-25 19:10:31 -0400

    my first py code

---
+++ lesson1.py
@@ -0,0 +1,28 @@
+pi=3.14159
+a=-5
+print(type(pi))
+'''
+x=float(input('enter a number '))
+y=float(input('enter a number '))
+#sum=int(x)+int(y)
+
+print(x+y)
+
+print(type(x))
+'''
+print(round(pi,4))
+
+print(abs(a))
+
+n1=2
+n2=5
+
+print(n1<n2)
+print(n1>n2)
+print(n1==n2)
+print(n1!=n2)
+print(n1<=n2)
+print(n1>=n2)
+
+question = (n1<n2)
+print(question)
